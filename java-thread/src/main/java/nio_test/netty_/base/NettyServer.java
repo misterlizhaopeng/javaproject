fbfee780de34d4168491d507658dde3af4eb3951
lip <misterlizhaopeng@163.com>
2020-07-06 11:55:43 +0800

    netty-server、netty-client基础测试

--- java-thread/src/main/java/nio_test/netty_/base/NettyServer.java
+++ java-thread/src/main/java/nio_test/netty_/base/NettyServer.java
@@ -1,4 +1,68 @@
 package nio_test.netty_.base;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 public class NettyServer {
+    public static void main(String[] args) {
+        //创建两个线程组bossGroup和workerGroup, 含有的子线程NioEventLoop的个数默认为cpu核数的两倍
+        // bossGroup只是处理连接请求 ,真正的和客户端业务处理，会交给workerGroup完成
+//        EventLoopGroup bossGroup = new NioEventLoopGroup(10);
+//        EventLoopGroup workerGroup = new NioEventLoopGroup(100000);
+
+        //一主多从模式
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            //创建服务器端的启动对象
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            //使用链式编程来配置参数
+            serverBootstrap.group(bossGroup, workGroup)//设置两个线程组
+                    .channel(NioServerSocketChannel.class)//使用NioServerSocketChannel作为服务器的通道实现
+                    // 初始化服务器连接队列大小，服务端处理客户端连接请求是顺序处理的,所以同一时间只能处理一个客户端连接。
+                    // 多个客户端同时来的时候,服务端将不能处理的客户端连接请求放在队列中等待处理
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            //对workerGroup的SocketChannel 设置处理器
+                            ch.pipeline().addLast(new NettyServerHandler());
+                        }
+                    });
+            System.out.println("netty server start...");
+            //绑定一个端口并且同步, 生成了一个ChannelFuture 异步对象，通过isDone()等方法可以判断异步事件的执行情况
+            //启动服务器(并绑定端口)，bind是异步操作，sync方法是等待异步操作执行完毕
+            ChannelFuture cf = serverBootstrap.bind(9100).sync();
+
+            //给cf注册监听器，监听我们关心的事件
+            /*cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (cf.isSuccess()) {
+                        System.out.println("监听端口9100 成功");
+                    } else {
+                        System.out.println("监听端口 9100 失败");
+                    }
+                }
+            });*/
+            //对通道关闭进行监听，closeFuture是异步操作，监听通道关闭
+            // 通过sync方法同步等待通道关闭处理完毕，这里会阻塞等待通道关闭完成
+            cf.channel().closeFuture().sync();
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+
+    }
 }
