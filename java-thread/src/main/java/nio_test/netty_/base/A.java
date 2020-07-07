a8ceac021443eda184da2ed2ed07abdab644f8cb
lip <misterlizhaopeng@163.com>
2020-07-08 02:57:17 +0800

    netty-heartbeat-ok

--- java-thread/src/main/java/nio_test/netty_/base/A.java
+++ java-thread/src/main/java/nio_test/netty_/base/A.java
@@ -1,4 +1,11 @@
 package nio_test.netty_.base;
 
+import io.netty.util.NettyRuntime;
+import io.netty.util.internal.SystemPropertyUtil;
+
 public class A {
+    public static void main(String[] args) {
+        System.out.println(SystemPropertyUtil.getInt(
+                "io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));;
+    }
 }
