e35c8ddabe47e568843346091b83e26e90fbd9ba
lip <misterlizhaopeng@163.com>
2021-02-22 11:02:26 +0800

    引用类型 ： 传递之后值的变化

---
+++ TestHW/src/main/java/MainTst.java
@@ -0,0 +1,51 @@
+import org.junit.Test;
+
+/**
+ * @ClassName PACKAGE_NAME.MainTst
+ * @Deacription TODO
+ * @Author LP
+ * @Date 2021/2/22 9:10
+ * @Version 1.0
+ **/
+public class MainTst {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    private static TestObj testObj;
+
+    @Test
+    public void testA() {
+        TestObj tt=new TestObj();
+        tt.setId(1);
+        tt.setName("xyz");
+        System.out.println(tt.toString());
+        testB(tt);
+        System.out.println(tt.toString());
+    }
+
+    private void testB(TestObj t) {
+
+//        TestObj testObj = new TestObj();
+//        testObj.setName("aaa");
+//        testObj.setId(123);
+
+
+//        if (a > 0) {
+//            TestObj testObj = new TestObj();
+//        }
+
+//        TestObj testObj = new TestObj();
+//        testObj.setName("aaa");
+//        testObj.setId(123);
+
+        t.setName("az");
+
+        System.out.println(t.toString());
+
+
+    }
+}
+
--- TestHW/src/main/java/TestObj.java
+++ TestHW/src/main/java/TestObj.java
@@ -37,5 +37,14 @@ public class TestObj {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public String toString() {
+        return "TestObj{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", to=" + to +
+                '}';
+    }
 }
 
