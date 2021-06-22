ee91d5012a9b9ecf0b6b478e2ab8b0fe70ae102f
lip <misterlizhaopeng@163.com>
2021-06-22 21:57:01 +0800

    channel update

--- java-thread/src/main/java/nio_test/nio_socket/channel/Channel_8_ByteChannel.java
+++ java-thread/src/main/java/nio_test/nio_socket/channel/Channel_8_ByteChannel.java
@@ -6,8 +6,8 @@ import org.junit.Test;
  * @ClassName nio_test.nio_socket.channel.Channel_8_ByteChannel
  * @Deacription :ByteChannel 接口：本身没有增加任何方法，只是同时继承了接口 ReadableByteChannel、WritableByteChannel;
  *                                 这样，ByteChannel接口就同时有了即可以写又可以读的 双向 的功能；
- *                                 ReadableByteChannel接口  只能从管道channel中读到缓冲区；
- *                                 WritableByteChannel接口 只能从缓冲区中写入管道中；
+ *                                 ReadableByteChannel接口  只能从管道channel中读到缓冲区 单向操作；
+ *                                 WritableByteChannel接口 只能从缓冲区中写入管道中  单向操作；
  *
  *
  * @Author LP
