ad99de7ed5cda43c14c9464a0979d63a531bb2b8
lip <misterlizhaopeng@163.com>
2021-06-23 21:56:09 +0800

    channel 11个接口-ok-fixed

--- java-thread/src/main/java/nio_test/nio_socket/channel/Channel_12_InterruptibleChannel.java
+++ java-thread/src/main/java/nio_test/nio_socket/channel/Channel_12_InterruptibleChannel.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 /**
  * @ClassName nio_test.nio_socket.channel.Channel_12_InterruptibleChannel
  * @Deacription : InterruptibleChannel 使通道能以异步的方式进行关闭和中断 ;
- *                  当通道实现了 asynchronously、closeable 特性，如果 一个线程 在可以被中断的通道上出现了阻塞，那么当其他线程调用这个通道的close方法时，这个阻塞的 线程会受到 AsynchronousCloseException 异常；
+ *                  当通道实现了 asynchronously、closeable 特性，如果 一个线程 在可以被中断的通道上出现了阻塞，那么当其他线程调用这个通道的close方法时，这个阻塞的 线程会收到 AsynchronousCloseException 异常；
  *                  当通道实现了 asynchronously、closeable 特性的同时还实现了 interrupt特性，如果 一个线程 在可以被中断的通道上出现了阻塞，那么当其他线程调用该阻塞线程的interrupt方法后，通道将关闭，阻塞线程将会收到 ClosedByInterruptException 异常；
  *
  *
